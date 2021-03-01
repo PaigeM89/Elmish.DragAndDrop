b892d9027c7836e3ad47f8277e5c8001e815f95d
Max Paige <max.paige@binarydefense.com>
2021-02-28 20:02:36 -0500

    Bump version to 0.1.0
    ## [0.1.0] - 2021-02-28
    
    ### Added
    - Drag and Drop with Rotate.
    - Horizontal, Vertical, and Free ghost item movement.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,14 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.1.0] - 2021-02-28
 
 ### Added
 - Drag and Drop with Rotate.
 - Horizontal, Vertical, and Free ghost item movement.
-
-
+[Unreleased]: https://github.com/MyGithubUsername/Elmish.DragAndDrop/compare/v0.1.0...HEAD
+[0.1.0]: https://github.com/MyGithubUsername/Elmish.DragAndDrop/releases/tag/v0.1.0
--- src/Elmish.DragAndDrop/AssemblyInfo.fs
+++ src/Elmish.DragAndDrop/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Elmish.DragAndDrop")>]
 [<assembly: AssemblyProductAttribute("Elmish.DragAndDrop")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-02-28T00:00:00.0000000-05:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","9e2f72b80a85b4fdcb32fd69f961b90b099836cb")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Elmish.DragAndDrop"
     let [<Literal>] AssemblyProduct = "Elmish.DragAndDrop"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-02-28T00:00:00.0000000-05:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "9e2f72b80a85b4fdcb32fd69f961b90b099836cb"
