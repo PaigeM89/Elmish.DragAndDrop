6c5e7f542c604d4a605ee94c76f9c9efcae65b66
Max Paige <max.paige@binarydefense.com>
2021-05-18 21:49:09 -0400

    Working handles example

--- Example/Example/src/Pages/HandlesDemo.fs
+++ Example/Example/src/Pages/HandlesDemo.fs
@@ -8,29 +8,36 @@ module HandlesDemo =
   open Browser.Types
   open Elmish
   open Elmish.React
-  open Elmish.DragAndDrop3
+  open Elmish.Handles
+
+
+  type ContentKey = string
+  type ContentValue = { UserInput : string; Name : string}
 
   type Model = {
-    DragAndDrop : DragAndDrop3.Model
-    Content : (string * ReactElement) list
-    ContentMap : Map<string, ReactElement>
+    DragAndDrop : Handles.Model
+    //Content : (string * string) list
+    ContentMap : Map<ContentKey, ContentValue>
+    //InputValueMap: Map<string, string>
   }
 
   let init() = 
     {
-      DragAndDrop = DragAndDrop3.empty()
-      Content = []
+      DragAndDrop = Handles.empty()
+      //Content = []
       ContentMap = Map.empty
+      //InputValueMap = Map.empty
     }
 
   type Msg =
   | Init
-  | DndMsg of DragAndDrop3.Msg
+  | DndMsg of Handles.Msg
+  | InputChange of elementId : string * newValue : string
 
   let mappedMsg msg = DndMsg msg
 
   let createDraggableTemplate className = {
-    DragAndDrop3.DraggableTemplate.Empty() with
+    Handles.DraggableTemplate.Empty() with
       DraggedElementStyles = Some [
           MarginLeft -130.
           MarginTop -50.
@@ -39,62 +46,100 @@ module HandlesDemo =
           Cursor "grabbing"
       ]
       DraggedElementProperties = Some [
-        ClassName (className + " dragged")
+        ClassName (className + " li-dragged")
       ]
       DraggableElementStyles = Some [
-        Cursor "grab"
+//        Cursor "grab"
       ]
       HoverPreviewElementStyles = Some [
         Opacity 0.2
-        PointerEvents "None"
+        // PointerEvents "None"
       ]
       // SlidingElementStyles = Some [
       //   CSSProp.TransitionDuration 1.0
       //   PointerEvents "None"
       //   Position PositionOptions.Fixed
       // ]
-      DefaultClass = Some "content"
+      DefaultClass = Some className
+      // HandlesStyles = Some [
+      //   Cursor "grab"
+      //   // Left "35px"
+      //   // Top "50px"
+      //   // CSSProp.Float FloatOptions.Left
+      //   // Position PositionOptions.Relative
+      //   Left 0
+      //   Top 0
+      //   Position PositionOptions.Absolute
+      //   BackgroundColor "3fd4d8"
+      // ]
+      // Handle = Html.em [ str "Grab me!" ] |> Some
   }
 
+
+  let inputValueLookup model elementId =
+    Map.tryFind elementId model.ContentMap
+    |> Option.defaultValue { UserInput = ""; Name = "Unknown input"}
+
+
+  let genContent msging inputId dispatch value =
+    [
+      DragHandle.dragHandle msging [ Style [ Cursor "grab" ]] [ h3 [] [ str (value.Name)] ]
+      input [
+        Id inputId
+        DefaultValue value.UserInput
+        OnChange (fun ev -> 
+          let v = ev.Value 
+          InputChange (inputId, v) |> (dispatch))
+      ]
+    ]
+
   let view model (dispatch : Msg -> unit) =
-    let template = createDraggableTemplate "content"
+    let template = createDraggableTemplate "li-content"
     let dropAreaProps =
       [
         (ClassName "container") :> IHTMLProp
       ] |> AreaProps
     let dropAreaContent =
       model.DragAndDrop.ElementIds()
-      |> List.map(fun li ->
+      |> List.map (fun li ->
         li
-        |> List.map (fun id ->
-          let content = model.ContentMap.Item id
+        |> List.map (fun (id) ->
           let msging = Messaging.Create model.DragAndDrop id (mappedMsg >> dispatch)
-          (msging, [content])
+          let content = inputValueLookup model id |> genContent msging id dispatch
+
+          msging, content
         )
         |> DropArea.dropArea model.DragAndDrop (mappedMsg >> dispatch) [dropAreaProps] template
       )
-    div [ ClassName "wrapper" ] dropAreaContent
+    div [
+      ClassName "backdrop"
+    ][
+      div [ ClassName "wrapper" ] dropAreaContent
+    ]
 
   let update msg model =
     match msg with
     | Init ->
       printfn "in init"
-      let content =
-        [
-          div [] [ h3 [] [str "Content 0"]; p [] [ str "This is some content" ]; input [] ]
-          div [] [ h3 [] [str "Content 1"]; p [] [ str "And this is more content" ]; input [] ]
-          div [] [ h3 [] [str "Content 2"]; p [] [ str "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ] ]
-          div [] [ h3 [] [str "Content 3"]; p [] [ str "And this is yet more content" ]; input [] ]
-          div [] [ h3 [] [str "Content 4"]; p [] [ str "some piece of content" ]; input [] ]
-
-          div [] [ h3 [] [str "Content 5"]; p [] [ str "hello world" ] ]
-          div [] [ h3 [] [str "Content 6"]; p [] [ str "this is another column" ] ]
-          div [] [ h3 [] [str "Content 7"]; p [] [ str "content can be anything - including images"]; img [Src "marek-piwnicki-unsplash.jpg"; Style [ Width "128px"; Height "128px" ]] ]
-        ] |> List.mapi (fun i c -> (sprintf "content-%i" i), c)
-      let ids = content |> List.map fst
-      let m = Map.ofList content
-      let dndModel = DragAndDrop3.ModelFuncs.createWithItems ids
-      { model with Content = content; DragAndDrop = dndModel; ContentMap = m }, Cmd.none
+      let content = [
+        for i in 1..7 do 
+          yield (sprintf "input-%i" i), { UserInput = ""; Name = sprintf "Input %i" i }
+      ]
+      let elementIds = content |> List.map fst
+      let m = content |> Map.ofList
+      let dndModel = Handles.ModelFuncs.createWithItems elementIds
+      { model with DragAndDrop = dndModel; ContentMap = m }, Cmd.none
     | DndMsg msg ->
-      let dndModel, cmd = DragAndDrop3.update msg model.DragAndDrop
+      let dndModel, cmd = Handles.update msg model.DragAndDrop
       { model with DragAndDrop = dndModel }, Cmd.map DndMsg cmd
+    | InputChange (elementId, newValue) ->
+      printfn "input change: %A value %A" elementId newValue
+      match Map.tryFind elementId model.ContentMap with
+      | Some oldContent ->
+        let newValue = { oldContent with UserInput = newValue}
+        let m = Map.add elementId newValue model.ContentMap
+        { model with ContentMap = m }, Cmd.none
+      | None ->
+        let newValue = { UserInput = newValue; Name = "" }
+        let m = Map.add elementId newValue model.ContentMap
+        { model with ContentMap = m }, Cmd.none
